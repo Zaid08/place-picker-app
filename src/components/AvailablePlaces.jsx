a421cfc961521b96776c6f4f7f3f555cc124b3f0
Zaid08 <hammadzaid08@gamil.com>
2024-06-30 10:10:11 +0000

    first commit

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,50 @@
+import Places from './Places.jsx';
+import Error from './Error.jsx';
+import {sortPlacesByDistance} from '../loc.js'
+import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places, 
+        position.coords.latitude, 
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  })
+}
+
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [availablePlaces, setAvailablePlaces] = useState([]);
+  // const [error, setError] = useState();
+
+  const {
+    isFetching, 
+    error, 
+    fetchData: availablePlaces, 
+  } = useFetch(fetchSortedPlaces, []);
+  
+ 
+  if (error) {
+    return <Error title='An error occured!' message = {error.message}></Error>
+  }
+  
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching the place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
